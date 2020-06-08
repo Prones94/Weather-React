c6c1d2a8402f75f6a300238dc4077b5d5d8fd500
Ian Rones <ronesp.85@gmail.com>
2020-06-08 12:09:07 -0700

    Updates

---
+++ precache-manifest.c71e4b263b8484a743d3e0edcdddc6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf136fe61b09dec6489e629a37267fc",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "431628ed77345ef03231",
+    "url": "/weather-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "676598109a4688cc829b",
+    "url": "/weather-api/static/js/2.ac723c82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-api/static/js/2.ac723c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431628ed77345ef03231",
+    "url": "/weather-api/static/js/main.7b747918.chunk.js"
+  },
+  {
+    "revision": "6a577383c1743a2d7456",
+    "url": "/weather-api/static/js/runtime-main.04ca8f8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
